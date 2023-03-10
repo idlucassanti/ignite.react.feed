31e72e8aa897c48ddd482bf374bd853ed706b105
Lucas Santi <idlucassanti@hotmail.com>
2023-03-10 14:39:17 -0300

    [02] CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { Header } from './components/Header';
 import { Post } from './components/Post';
 
 export function App() {
   return (
     <div>
+      <Header />
+
       <Post author="Lucas Santi" content="Conteúdo teste 1 LS"/>
       <Post author="Matteo Santi" content="Conteúdo teste 2 MS"/>
       <Post author="Dyovana Santi" content="Conteúdo teste 3 DS"/>
