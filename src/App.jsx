450067a92aa902f509313f26523b3209a9ca0e7a
Lucas Santi <idlucassanti@hotmail.com>
2023-03-12 04:27:59 -0300

    [06] Componente Sidebar com Ícone SVG Externo

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import './global.css';
-import { Header } from './components/Header';
-import { Post } from './components/Post';
+import styles from './App.module.css'
+import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
+import { Post } from './components/Post'
 
 export function App() {
   return (
     <div>
       <Header />
 
-      <Post author="Lucas Santi" content="Conteúdo teste 1 LS"/>
-      <Post author="Matteo Santi" content="Conteúdo teste 2 MS"/>
-      <Post author="Dyovana Santi" content="Conteúdo teste 3 DS"/>
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <div>
+          <Post author="Lucas Santi" content="Conteúdo teste 1 LS"/>
+          <Post author="Matteo Santi" content="Conteúdo teste 2 MS"/>
+          <Post author="Dyovana Santi" content="Conteúdo teste 3 DS"/>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,23 @@
+import styles from './Sidebar.module.css'
+import { PencilSimpleLine } from '@phosphor-icons/react'
+
+export function Sidebar(props) {
+
+  return(
+    <aside className={styles.sidebar}>
+      <img className={styles.backgroundImage} src="https://images.unsplash.com/photo-1557682250-33bd709cbe85?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" alt="" />
+      
+      <div className={styles.profileInfo}>
+        <img src="https://avatars.githubusercontent.com/u/46196863?s=400&u=33715344194a8e048f219990279b0bbcde31f6c8&v=4" alt="Imagem Perfil" />
+
+        <span>Lucas Santi</span>
+        <strong>.NET Developer</strong>
+      </div>
+
+      <footer>
+        <a href=''>
+          <PencilSimpleLine size={20} />Editar seu perfil</a>
+      </footer>
+    </aside>
+  )
+}
